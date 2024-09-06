b84de991316ef03c457ca5b4a20a7285d0ee0c22
leticiapzs <leticia_95lima@hotmail.com>
2024-09-06 09:57:03 -0300

    operações matemáticas e anotação de estudos

---
+++ operacoes-matematicas/src/Subtracao.java
@@ -0,0 +1,8 @@
+public class Subtracao {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int subtracao = a-b;
+        System.out.println(subtracao);
+    }
+}
